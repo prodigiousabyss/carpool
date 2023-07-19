819a9e21cd1b7ba2f908faaa57608fe354a2bb65
Pooja <poojaghanghas479@gmail.com>
2023-07-20 01:27:01 +0530

    console

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,7 +5,9 @@ const highest = 8;
 const lowest = 1;
 
 export const Home = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
+  const [leaving, setLeaving] = useState("");
+  const [going, setGoing] = useState("");
 
   const increaseCount = () => {
     if (count < highest) setCount(count + 1);
@@ -13,15 +15,27 @@ export const Home = () => {
   const decreaseCount = () => {
     if (count > lowest) setCount(count - 1);
   };
+   
+  const handleLeaving = (event) => {
+    setLeaving(event.target.value);
+  }
+
+  const handleGoing = (event) => {
+    setGoing(event.target.value);
+  }
+
+  const handleSearch = () => {
+    console.log(count, leaving, going);
+  }
 
   return (
     <div className="w-full pt-1.5 pl-14 flex items-center flex-1">
       <div className="h-[35rem] bg-white drop-shadow-md w-96 rounded-2xl pt-7 flex flex-col items-center">
         <p className="m-4 font-bold text-lg">Where would you like to go?</p>
-        <input type="text" placeholder="Leaving from..." className="bg-gray-100 w-80 h-11 rounded-lg pl-4 border-2" />
-        <input type="text" placeholder="Going to..." className="bg-gray-100 w-80 h-11 rounded-lg pl-4 mt-6 border-2" />
+        <input type="text" value ={leaving} placeholder="Leaving from..." className="bg-gray-100 w-80 h-11 rounded-lg pl-4 border-2" onChange={handleLeaving} />
+        <input type="text" value ={going} placeholder="Going to..." className="bg-gray-100 w-80 h-11 rounded-lg pl-4 mt-6 border-2" onChange={handleGoing}/>
         <div className="mt-6">
-          <div className="text-black w-72 h-11 bg-gray-100 rounded-lg border-2">
+          <div className="text-black w-72 h-11 bg-gray-100 rounded-lg border-2" >
             {count === 1 ? 'Passenger' : 'Passengers'}
             <button className="m-2 text-white bg-gray-400 rounded-full w-6 h-6" onClick={decreaseCount}>
               -
@@ -32,14 +46,15 @@ export const Home = () => {
             </button>
           </div>
         </div>
-        <DatePicker
+        <DatePicker 
           className="mt-6"
           label="Select date"
           sx={{
             margin: 4,
-          }}
-        />
-        <button className="mt-6 bg-black text-white w-72 h-11 rounded-md">Search</button>
+          }}/>
+        <button className="mt-6 bg-black text-white w-72 h-11 rounded-md"
+        onClick={handleSearch}
+        >Search</button>
       </div>
     </div>
   );
