6d1c5ca15b0f308070ee72e64b26c9cf7aee8cf5
Pooja <poojaghanghas479@gmail.com>
2023-08-07 18:29:13 +0530

    request rides complete

--- src/pages/RequestRide/requestRides.js
+++ src/pages/RequestRide/requestRides.js
@@ -1,3 +1,6 @@
+import riding from "../../Images/Ride-with-Uber.webp";
+import Car from "../../Images/car.webp";
+
 export const RequestRides = () => {
 
     const rideOptions = [
@@ -9,6 +12,7 @@ export const RequestRides = () => {
             GoingTo: "Jaipur",
             LeavingTime: "6:00PM",
             ArrivingTime: "10:00PM",
+            rating: "5 ★",
         },
         {
             driverName: "Olive",
@@ -18,6 +22,7 @@ export const RequestRides = () => {
             LeavingTime: "6:00PM",
             ArrivingTime: "10:00PM",
             tripFare: "400₹",
+            rating: "5 ★",
         },
         {
             driverName: "Slyvia",
@@ -27,6 +32,7 @@ export const RequestRides = () => {
             LeavingTime: "6:00PM",
             ArrivingTime: "10:00PM",
             tripFare: "300₹",
+            rating: "5 ★",
         },
         {
             driverName: "Ash",
@@ -36,33 +42,57 @@ export const RequestRides = () => {
             LeavingTime: "6:00PM",
             ArrivingTime: "10:00PM",
             tripFare: "600₹",
+            rating: "5 ★",
         }
     ]
 
 
     return (
         <div className="flex justify-center w-full h-full my-4">
-            <div>Left</div>
+            <div className="w-[20rem] text-left sticky top-0 mx-16">
+            <img src={Car} alt="woman" className="" />
+            <p className="font-bold text-xl my-2">
+            Ways people move around the world!
+            </p>
+            <p className="font-normal text-md">
+            The Carpool app gives you the power to get where you want to go.
+            </p>
+            </div>
             <div>
         {rideOptions.map(rd => 
-        <div className="border-2 w-[36rem] h-[10rem] my-4 rounded-xl"> 
-            <div className="flex flex-col">
-                    <div className="">{rd.LeavingTime}</div>
+        <div className="border-2 w-[36rem] h-[10rem] my-4 rounded-xl drop-shadow-sm"> 
+            <div className="flex items-start px-4 mt-4">
+                <div className="space-y-2">
+                    <div>{rd.LeavingTime}</div>
                     <div>{rd.ArrivingTime}</div>
-                <div>
-                    <div>{rd.LeavingFrom}</div>
-                    <div>{rd.GoingTo}</div>
-                </div>
-                    <div>{rd.tripFare}</div>
-                <div></div>
+                    </div>
+                    <div className="relative mx-6 flex flex-col items-start">
+                        <div className="bg-black w-0.5 top-2 h-9 absolute left-[0.16rem]" />
+                        <div className="flex items-center">
+                            <div className="w-2 h-2 rounded-full bg-black mr-2" />
+                            {rd.LeavingFrom}
+                        </div>
+                        <div className="flex items-center mt-2">
+                            <div className="w-2 h-2 rounded-full bg-black mr-2" />
+                            {rd.GoingTo}
+                        </div>
+                    </div>
+                    <div className="font-medium px-[20rem]">{rd.tripFare}</div>
             </div>
-            <div className="flex mt-4 px-[0.7rem] space-x-[22rem] font-medium">
-                <div>{rd.driverName}</div>
-                <div>{rd.isVerified}</div>
+            <div className="flex flex-col items-start mt-4 px-[1rem]">
+                <div className="flex flex-row space-x-4">
+                <div className="font-medium">{rd.driverName}</div>
+                </div>
+                <div className="font-normal">{rd.rating}</div>
             </div>
         </div>)}
             </div>
-            <div>Right</div>
+            <div className="w-[20rem] text-left sticky top-0 mx-16">
+            <img src={riding} alt="car" className="" />
+            <p className="my-2">
+            Whether you're running an errand across town or exploring a city far from home, getting there should be easy. 
+            </p>
+            </div>
         </div>
     )
 }
