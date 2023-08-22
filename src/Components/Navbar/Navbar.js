3e65cab16d9787bf78139fb3e81c58c476a115e3
Pooja <poojaghanghas479@gmail.com>
2023-08-23 01:06:30 +0530

    login route set

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ export const Navbar = () => {
   const [show, setShow] = useState(false);
 
   return (
-    <div className="text-white bg-black drop-shadow-md h-16 flex items-center px-8 justify-between z-50">
+    <div className="text-white bg-black drop-shadow-md h-16 flex items-center px-8 justify-between z-40">
       <div className="flex items-center">
         <NavLink to="/">
           <Logo className="h-max w-24 left-0 m-4" />
@@ -24,7 +24,7 @@ export const Navbar = () => {
         </NavLink>
       </div>
       <div>
-        <NavLink to="/login" className="relative" onClick={() => setShow((value) => !value)}>
+        <NavLink to="/login" >
           Log In
         </NavLink>
         <Modal show={show} setShow={setShow} />
--- src/pages/LogIn/LogIn.js
+++ src/pages/LogIn/LogIn.js
@@ -1,7 +1,19 @@
 export const LogIn = () => {
 
     return (
-        <div>
+        <div className="z-50 h-full w-full flex justify-center flex-col mt-2">
+            <div className="flex flex-col">
+                <p className="font-bold text-xl my-4">What's your phone number or email?</p>
+                <div className="my-1">
+                <input type="email" placeholder="Enter phone number or email" className="w-[26rem] h-12 rounded-md pl-1" />
+                </div>
+                <div className="my-1">
+                <input type="password" placeholder="password" className="w-[26rem] h-12 rounded-md pl-1" />
+                </div>
+                <div className="my-8">
+                <button className="text-white bg-black p-2 rounded-md w-[14rem]">Continue</button>
+                </div>
+            </div>
         </div>
     )
 }
